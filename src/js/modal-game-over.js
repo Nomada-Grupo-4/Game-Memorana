6e7d1baf582081b4d56b09bb7f9731aa0d0363b9
antony.sl <antony.g.s.lopez@gmail.com>
2025-03-13 01:03:15 -0500

    Se agrego funcion para distribuir resumen de juego, agregar o mostrar modal y simulacion de funcion con setTimeout para mostrar estadisticas luego de 4 segundos junto a boton para mostrar la misma

--- src/js/modal-game-over.js
+++ src/js/modal-game-over.js
@@ -1,10 +1,39 @@
 //Temporizador
-let tiempoRestante = 6;
+let tiempoRestante = 5;
+
+//arreglo EJEMPLO para exponer modal GameOver
+const resultados = {
+    points: 350,
+    time: 2,
+    acerts: 10,
+    totalAcerts: 12
+}
+
 const tiempo = document.getElementById("tiempo");
 const modalGameOver = document.getElementById("modaL-GameOver");
 
+//funcion para cargados de resumen de juego a modal GameOver
+function cargarDatosGO (){
+    
+    //obtenemos los id para modificar
+    const totalPuntos = document.getElementById("total-puntos");
+    const puntos = document.getElementById("points");
+    const tiempo = document.getElementById("time");
+    const aciertos = document.getElementById("acerts")
+    //destructuramos el objeto resutados
+    const {points, time, acerts, totalAcerts} = resultados;
+    //agregamos nuevos valores
+    totalPuntos.innerHTML = `${points*(acerts*time)}`;
+    puntos.innerHTML = `${points}`;
+    tiempo.innerHTML = `${time}`;
+    aciertos.innerHTML = `${acerts}/${totalAcerts}`;
+}
+//funcion para mostrar u ocultar modal
+function mostrarQuitarModal (modal){
+    modal.classList.toggle("hidden");
+}
 let contador = null;
-
+//funcion para corre temporizador e inicar juego
 function correrTiempo () {
     contador = setInterval(()=> {
         if(tiempoRestante > 1){
@@ -12,9 +41,16 @@ function correrTiempo () {
             tiempo.innerText = `${tiempoRestante}`
         } else {
             clearInterval(contador);
-//Mostramos modal de GameOver
-        modalGameOver.classList.remove("hidden")
-        modalGameOver.classList.add("grid", "fixed", "z-[40]");
+        //Cargamos datos y mostramos modal GameOver
+            cargarDatosGO();
+            mostrarQuitarModal(modalGameOver);
+            console.log(`mostrar modal`);
+        //codigo para mostrar estadisticas
+        /*
+        setTimeout(()=>{
+            mostrarQuitarModal(modalGameOver);
+            mostrarQuitarModal(modalEstadisticas);
+        },4000)*/
         }
     },1000);
 }
@@ -37,12 +73,12 @@ function playPause(valor){
 //funcion para mostrar model cerrar juego
 function salirDeJuego(){
     const modalSalir = document.getElementById("modalSalir");
-    modalSalir.classList.remove("hidden")
-    modalSalir.classList.add("grid", "fixed", "z-[40]");
+    modalSalir.classList.toggle("hidden")
+    clearInterval(contador)
 }
 
 function regresarJuego(){
     const modalSalir = document.getElementById("modalSalir");
-    modalSalir.classList.remove("grid", "fixed", "z-[40]")
-    modalSalir.classList.add("hidden");  
+    modalSalir.classList.toggle("hidden");
+    correrTiempo();
 }
