1dd65d947c5cf33ac6ad62bf9532d892a676cd50
Flavio <flavioesccu96@gmail.com>
2025-03-28 00:22:47 -0500

    agregando html y js de modal-ranking

---
+++ src/js/mainmodal.js
@@ -0,0 +1,31 @@
+
+function openModal() {
+const modal = document.getElementById('myModal');
+
+if (modal) {
+    modal.classList.remove('hidden');
+    modal.classList.add('flex', 'items-center', 'justify-center');
+} else {
+    console.error('Modal element not found');
+}
+}
+
+function closeModal() {
+const modal = document.getElementById('myModal');
+
+if (modal) {
+    modal.classList.remove('flex', 'items-center', 'justify-center');
+    modal.classList.add('hidden');
+} else {
+    console.error('Modal element not found');
+}
+}
+
+// Mostrar el modal automáticamente después de 5 segundos (5000 ms)
+setTimeout(() => {
+openModal();
+}, 5000);
+
+//exponer las funciones al ambito global
+window.openModal = openModal;
+window.closeModal = closeModal;
